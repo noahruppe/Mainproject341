b9dbe29018ee99889516697604113044666c6a0f
noahruppe <noah.ruppe30@gmail.com>
2025-04-02 19:55:40 -0400

    changes

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.use("/", require("./routes/index"))
 
 const port = process.env.PORT || 3002;
 
+process.on("uncaughtException", (err, origin)=>{
+    console.log(process.stderr.fd, `caught exception: ${err}\n` * `Exception origin: ${origin}`);
+});
+
+
 mongodb.initdb((err) =>{
     if(err){
         console.log(err)
