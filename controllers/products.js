2098659a39cd5f3b3b9b326d173cb86db177aafd
AdongoNat <danieladongonat@gmail.com>
2025-04-17 21:54:54 +0000

    g

--- controllers/products.js
+++ controllers/products.js
@@ -13,7 +13,7 @@ const getAll = async (req, res) => {
         res.setHeader("Content-type", "application/json");
         res.status(200).json(products);
     } catch (err) {
-        res.status(500).json({ message: "An error occurred while fetching the products", error: err });
+        res.status(500).json({ message: "An error occurred while fetching thee products", error: err });
     }
 };
 
